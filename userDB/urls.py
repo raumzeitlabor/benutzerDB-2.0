244db4107a9d5bdf6fe1c0fbe5f1d313700d1c38
slowpoke <mail+git@slowpoke.io>
2017-11-26 21:14:44 +0000

    add basic bootstrap stuff

--- userDB/urls.py
+++ userDB/urls.py
@@ -8,6 +8,7 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    url(r'^$', auth_views.login, name="login"),
+    url(r'^$',views.index, name='index'),
+    url(r'^login$', auth_views.login, name="login"),
     url(r'^logout$', auth_views.logout, name="logout"),
 ]
--- userDB/views.py
+++ userDB/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("haro")
+    #return HttpResponse("haro")
+    return render(request, "index.html")
