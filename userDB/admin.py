69d37a448e293a3d3c3ef025b810c76c5b4a2355
Lukas Martini <hello@lutoma.org>
2018-03-05 00:40:11 +0100

    Prettify admin and make models i18n compatible

--- userDB/admin.py
+++ userDB/admin.py
@@ -4,11 +4,12 @@ from .models import *
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ("user",)
-    list_filter = ("member",)
-    readonly_fields = ("pin",)
+    list_display = ('user', 'member')
+    list_filter = ('member',)
+    readonly_fields = ('pin',)
+    search_fields = ('user__username', 'user__first_name', 'user__last_name')
 
 
 @admin.register(SSHKey)
 class SSHKeyAdmin(admin.ModelAdmin):
-    pass
+    readonly_fields = ('hash_md5',)
--- userDB/models.py
+++ userDB/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 from django.db.models.signals import post_save, pre_save
-
+from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import User
-
 from django.dispatch import receiver
-
 import random
 
 
@@ -19,10 +17,21 @@ class Profile(models.Model):
     # Whether or not this User is a member of the RaumZeitLabor e.V. This must
     # only be settable by administrators (the NOC) or board members (the
     # Vorstand).
-    member = models.BooleanField(default=False, blank=True)
-    # The PIN for accessing the room. This must be a 6 digit number.
-    pin = models.PositiveSmallIntegerField(null=True)
+    member = models.BooleanField(default=False, blank=True,
+                                 verbose_name=_('Member'))
 
+    # The PIN for accessing the room. This must be a 6 digit number.
+    pin = models.PositiveSmallIntegerField(
+        null=True, verbose_name=_('PIN'),
+        help_text=_('Will be automatically generated if user is set as member'))
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        ordering = ['user__username']
+        verbose_name = _('profile')
+        verbose_name_plural = _('profiles')
 
 # When a User is a member, automatically generate a random 6-digit PIN if there
 # is none yet.
@@ -50,6 +59,17 @@ class SSHKey(models.Model):
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE,
                                 related_name='ssh_keys')
 
-    name = models.CharField(max_length=200)
-    key = models.TextField()
-    hash_md5 = models.CharField(max_length=47)
+    name = models.CharField(max_length=200, verbose_name=_('Name'))
+    key = models.TextField(verbose_name=_('SSH key'))
+
+    hash_md5 = models.CharField(
+        max_length=47, verbose_name=_('MD5 hash'),
+        help_text=_('Automatically generated hash of the key'))
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        ordering = ['hash_md5']
+        verbose_name = _('SSH key')
+        verbose_name_plural = _('SSH keys')
