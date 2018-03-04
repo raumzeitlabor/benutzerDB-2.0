07ccded0e5d0f31c9525c8f5a90e68b1bf4996ea
slowpoke <mail+git@slowpoke.io>
2018-03-04 22:12:40 +0000

    Fix small regex error.

--- benutzerDB/urls.py
+++ benutzerDB/urls.py
@@ -36,7 +36,7 @@ router.register(r'users', UserViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    url(r'api/^', include(router.urls)),
+    url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', admin.site.urls),
     url(r'^', include('userDB.urls')),
