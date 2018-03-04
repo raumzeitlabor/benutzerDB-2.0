2d1f0814699f2c58c10671b909ee4bf01b8650b3
Lukas Martini <hello@lutoma.org>
2018-03-04 23:36:45 +0100

    Move login views to global urls.py

--- benutzerDB/urls.py
+++ benutzerDB/urls.py
@@ -1,22 +1,8 @@
-"""benutzerDB URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
+from django.contrib.auth import views as auth_views
 
 # Serializers define the API representation.
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -39,5 +25,7 @@ urlpatterns = [
     url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', admin.site.urls),
+    url(r'accounts/login/', auth_views.login, name="login"),
+    url(r'accounts/logout/', auth_views.logout, name="logout"),
     url(r'^', include('userDB.urls')),
 ]
--- userDB/urls.py
+++ userDB/urls.py
@@ -2,13 +2,8 @@
 # -*- coding: utf-8 -*-
 
 from django.conf.urls import url
-
-from django.contrib.auth import views as auth_views
-
 from . import views
 
 urlpatterns = [
     url(r'^$',views.index, name='index'),
-    url(r'accounts/login/', auth_views.login, name="login"),
-    url(r'accounts/logout/', auth_views.logout, name="logout"),
 ]
