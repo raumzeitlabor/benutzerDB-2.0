214b6dfd61b1546f4639e1f20a604ae9ab8eaf85
Lukas Martini <hello@lutoma.org>
2018-03-05 02:31:13 +0100

    Add internationalization

--- benutzerDB/settings.py
+++ benutzerDB/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -107,14 +108,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+ugettext = lambda s: s
+LANGUAGES = (
+    ('en', 'English'),
+    ('de', 'Deutsch'),
+)
 
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
