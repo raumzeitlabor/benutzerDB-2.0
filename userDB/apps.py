106818ecf947fc6eca4cbb7d26f6228faf05a793
slowpoke <mail+git@slowpoke.io>
2018-03-17 23:56:08 +0000

    Pinpad user is now automatically generated.
    It will use the respective settings from the settings_local file, or the
    development defaults otherwise.

--- userDB/apps.py
+++ userDB/apps.py
@@ -1,5 +1,14 @@
 from django.apps import AppConfig
+from benutzerDB import settings
 
 
 class UserdbConfig(AppConfig):
     name = 'userDB'
+
+    def ready(self):
+        from django.contrib.auth.models import User
+        if not User.objects.filter(username=settings.PINPAD_USERNAME).exists():
+            pinpad_user = User.objects.create_superuser(
+                username=settings.PINPAD_USERNAME,
+                password=settings.PINPAD_PASSWORD,
+                email="pinpad@tuer.rzl")
