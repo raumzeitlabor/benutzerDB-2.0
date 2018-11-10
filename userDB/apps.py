0e606d805605a1acc2b139677d4d2d8e92b4760b
slowpoke <mail+git@slowpoke.io>
2018-11-10 15:58:38 +0000

    Update Pipfile.lock.
    Disable automatic generation of pinpad user.

--- userDB/apps.py
+++ userDB/apps.py
@@ -7,8 +7,10 @@ class UserdbConfig(AppConfig):
 
     def ready(self):
         from django.contrib.auth.models import User
-        if not User.objects.filter(username=settings.PINPAD_USERNAME).exists():
-            pinpad_user = User.objects.create_superuser(
-                username=settings.PINPAD_USERNAME,
-                password=settings.PINPAD_PASSWORD,
-                email="pinpad@tuer.rzl")
+        # XXX: this errors if the db is not initialized.
+        # TODO: find a way around this
+        #if not User.objects.filter(username=settings.PINPAD_USERNAME).exists():
+            #pinpad_user = User.objects.create_superuser(
+                #username=settings.PINPAD_USERNAME,
+                #password=settings.PINPAD_PASSWORD,
+                #email="pinpad@tuer.rzl")
